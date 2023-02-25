3457abff6c5a510769ce7d0a5ce4b9a2a75a81b5
WideSpreadOS <84348146+WideSpreadOS@users.noreply.github.com>
2023-02-25 09:17:11 -0500

    updated landing page data - made logo smaller - added on to Note schema

--- models/Note.js
+++ models/Note.js
@@ -4,7 +4,12 @@ const NoteSchema = new mongoose.Schema({
     title: String,
     category: String,
     sub_category: [String],
+    tags: [String],
     body: String,
+    resrources: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Resource' }],
+    url_images: [String],
+    images: [String],
+    importance: Number,
     added: {
         type: Date,
         default: Date.now()
